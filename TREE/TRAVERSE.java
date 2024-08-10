4d6ddd6b6aa2f6710bb94a21c0dc6506181fe0c0
NIKHIL5457 <159000013+NIKHIL5457@users.noreply.github.com>
2024-08-11 00:55:44 +0530

    Update TRAVERSE.java
    This Java program defines a basic implementation of a binary tree with three different traversal methods: preorder, inorder, and postorder. Here's a more detailed description:
    
    preorder(node root):
    
    A method to perform preorder traversal (root, left, right) of the binary tree.
    Prints the data of each node as it traverses.
    
    inorder(node root):
    
    A method to perform inorder traversal (left, root, right) of the binary tree.
    Prints the data of each node as it traverses.
    
    postorder(node root):
    
    A method to perform postorder traversal (left, right, root) of the binary tree.
    Prints the data of each node as it traverses.

--- TREE/TRAVERSE.java
+++ TREE/TRAVERSE.java
@@ -34,11 +34,33 @@ public class basicpro {
             preorder(root.left);
             preorder(root.right);
         }
+         public static void inorder(node root) {
+            if (root == null) {
+                return;
+            }
+            inorder(root.left);
+            System.out.print(root.data);
+            inorder(root.right);
+        }
+
+        public static void postorder(node root) {
+            if (root == null) {
+                return;
+            }
+            postorder(root.left);
+            postorder(root.right);
+            System.out.print(root.data);
+
+        }
     }
    public static void main(String[] args) {
         int arr[] = { 1, 2, 3, -1, -1, 4, 5, -1, -1, 6, -1, -1, 6, 7, -1, -1, 8, -1, 9, -1, -1 };
         jkl s1 = new jkl();
         node root = s1.create(arr);
         s1.preorder(root);
+        System.out.println("");
+       s1.inorder(root);
+       System.out.println("");
+       s1.postorder(root);
    }
 }
