aafd79e1587e49f7fff4a0d43fdccc60b4d9379e
NIKHIL RAJPUT <159000013+NIKHIL5457@users.noreply.github.com>
2024-08-19 22:33:07 +0530

    Create L1642.java
    You are given an integer array heights representing the heights of buildings, some bricks, and some ladders.
    
    You start your journey from building 0 and move to the next building by possibly using bricks or ladders.
    
    While moving from building i to building i+1 (0-indexed),
    
    If the current building's height is greater than or equal to the next building's height, you do not need a ladder or bricks.
    If the current building's height is less than the next building's height, you can either use one ladder or (h[i+1] - h[i]) bricks.
    Return the furthest building index (0-indexed) you can reach if you use the given ladders and bricks optimally.

---
+++ HEAP/L1642.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> s=new PriorityQueue<>();
+        for(int i=1;i<heights.length;i++){
+            int diff=heights[i]-heights[i-1];
+            if(diff>0){
+                if(s.size()<ladders){
+                    s.add(diff);
+                }
+                else{
+                    int br=diff;
+                    if(s.size()>0 && s.peek()<diff){
+                        br=s.remove();
+                        s.add(diff);
+                    }
+                    if(bricks-br>=0){
+                        bricks-=br;
+                    }else{
+                        return i-1;
+                    }
+                }
+            }
+        }
+        return heights.length-1;
+    }
+}
